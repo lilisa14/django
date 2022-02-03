d876a178849238d207e41f950bb8152dc1fa82f1
Your Name <you@example.com>
2022-02-03 16:08:07 +0500

    4

--- main/views.py
+++ main/views.py
@@ -1,11 +1,16 @@
 from django.shortcuts import render
+from .models import Task
 
 
 def index(request):
     return render(request, 'main/index.html')
 
 def about(request):
-    return render(request, 'main/about.html')
+    tasks = Task.objects.all()
+    return render(request, 'main/about.html', {'tasks': tasks})
 
-def links(request):
+def list(request):
     return render(request, 'main/list.html')
+
+def mmain(request):
+    return render(request, 'main/mmain.html')
